f30e77b6a5417d9f503b95080f219eae26389bb3
Fábio Correia <fabio.correia@moloni.com>
2021-05-11 16:01:48 +0100

    Added jquery library
    The plugin now enques jquery library

--- src/Plugin.php
+++ src/Plugin.php
@@ -35,9 +35,11 @@ class Plugin
 
             wp_enqueue_style('jquery-modal', plugins_url('assets/external/jquery.modal.min.css', MOLONI_ES_PLUGIN_FILE));
             wp_enqueue_script('jquery-modal', plugins_url('assets/external/jquery.modal.min.js', MOLONI_ES_PLUGIN_FILE));
+            wp_enqueue_script('jquery', plugins_url('assets/external/jquery-3.6.0.min.js', MOLONI_ES_PLUGIN_FILE));
 
             wp_enqueue_style('moloni-styles', plugins_url('assets/css/moloni.css', MOLONI_ES_PLUGIN_FILE));
             wp_enqueue_script('moloni-actions-bulk-documentes-js', plugins_url('assets/js/BulkDocuments.js', MOLONI_ES_PLUGIN_FILE));
+
             //send translated strings to the javascript file
             wp_localize_script('moloni-actions-bulk-documentes-js', 'translations', [
                 'startingProcess' => __('Starting process...', 'moloni_es'),
