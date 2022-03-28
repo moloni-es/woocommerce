c344055282f257ddb5d41ef1453a52169424cc6b
Fábio Correia <fabio.correia@moloni.com>
2022-03-28 11:32:29 +0100

    Fix warnings

--- src/Scripts/Enqueue.php
+++ src/Scripts/Enqueue.php
@@ -14,9 +14,10 @@ class Enqueue
      * Define some table params
      * Load scripts and CSS as needed
      */
-    public function defines()
+    public static function defines()
     {
         if (isset($_REQUEST['page']) && !wp_doing_ajax() && sanitize_text_field($_REQUEST['page']) === 'molonies') {
+            $tab = isset($_GET['tab']) ? $_GET['tab'] : '';
 
             wp_enqueue_script('jquery', plugins_url('assets/external/jquery-3.6.0.min.js', MOLONI_ES_PLUGIN_FILE));
             wp_enqueue_style('jquery-modal', plugins_url('assets/external/jquery.modal.min.css', MOLONI_ES_PLUGIN_FILE));
@@ -24,11 +25,11 @@ class Enqueue
 
             wp_enqueue_style('moloni-styles', plugins_url('assets/css/moloni.css', MOLONI_ES_PLUGIN_FILE));
 
-            if ($_GET['tab'] === 'settings') {
+            if ($tab === 'settings') {
                 wp_enqueue_script('moloni-settings-js', plugins_url('assets/js/Moloni.Settings.js', MOLONI_ES_PLUGIN_FILE));
             }
 
-            if (!in_array($_GET['tab'], ['settings', 'tools', 'automation'])) {
+            if (!in_array($tab, ['settings', 'tools', 'automation'])) {
                 wp_enqueue_script('moloni-actions-bulk-documentes-js', plugins_url('assets/js/BulkDocuments.js', MOLONI_ES_PLUGIN_FILE));
 
                 //send translated strings to the javascript file
