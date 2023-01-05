d0744583bd5a987bef1f346c0f051006f433da2d
Fábio Correia <fabio.correia@moloni.com>
2023-01-05 15:07:33 +0000

    Fix PHP 8 ternary

--- src/Controllers/OrderShipping.php
+++ src/Controllers/OrderShipping.php
@@ -176,7 +176,6 @@ class OrderShipping
     private function setDiscount()
     {
         $this->discount = $this->price <= 0 ? 100 : 0;
-        $this->discount = $this->discount < 0 ? 0 : $this->discount > 100 ? 100 : $this->discount;
 
         return $this;
     }
