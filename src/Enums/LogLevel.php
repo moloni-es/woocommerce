165b51fb723c8d8adf82897f5182983c491e2404
Fábio Correia <fabio.correia@moloni.com>
2023-05-17 15:10:01 +0100

    Improve logs

--- src/Enums/LogLevel.php
+++ src/Enums/LogLevel.php
@@ -5,13 +5,47 @@ namespace MoloniES\Enums;
 class LogLevel
 {
     const EMERGENCY = 'emergency';
-    const ALERT     = 'alert';
-    const CRITICAL  = 'critical';
-    const ERROR     = 'error';
-    const WARNING   = 'warning';
-    const NOTICE    = 'notice';
-    const INFO      = 'info';
-    const DEBUG     = 'debug';
+    const ALERT = 'alert';
+    const CRITICAL = 'critical';
+    const ERROR = 'error';
+    const WARNING = 'warning';
+    const NOTICE = 'notice';
+    const INFO = 'info';
+    const DEBUG = 'debug';
+
+    public static function getForRender(): array
+    {
+        return [
+            [
+                'label' => __('Error'),
+                'value' => self::ERROR
+            ],
+            [
+                'label' => __('Warning'),
+                'value' => self::WARNING
+            ],
+            [
+                'label' => __('Informative'),
+                'value' => self::INFO
+            ],
+            [
+                'label' => __('Alert'),
+                'value' => self::ALERT
+            ],
+            [
+                'label' => __('Critical'),
+                'value' => self::CRITICAL
+            ],
+            [
+                'label' => __('Emergency'),
+                'value' => self::EMERGENCY
+            ],
+            [
+                'label' => __('Observation'),
+                'value' => self::NOTICE
+            ]
+        ];
+    }
 
     public static function getTranslation(string $type): ?string
     {
@@ -32,6 +66,26 @@ class LogLevel
                 return __('Emergency');
             case self::NOTICE:
                 return __('Observation');
+        }
+
+        return $type;
+    }
+
+    public static function getClass(string $type): ?string
+    {
+        switch ($type) {
+            case self::CRITICAL:
+            case self::EMERGENCY:
+            case self::ERROR:
+                return 'chip--red';
+            case self::ALERT:
+            case self::WARNING:
+                return 'chip--yellow';
+            case self::NOTICE:
+            case self::INFO:
+                return 'chip--blue';
+            case self::DEBUG:
+                return 'chip--neutral';
         }
 
         return $type;
--- src/Templates/Containers/Logs.php
+++ src/Templates/Containers/Logs.php
@@ -19,55 +19,106 @@ $logs = Logs::getAllAvailable();
         </div>
     </div>
 
-    <table class='wp-list-table widefat fixed striped posts'>
-        <thead>
-        <tr>
-            <th><a><?= __('Date', 'moloni_es') ?></a></th>
-            <th><a><?= __('Level', 'moloni_es') ?></a></th>
-            <th><a><?= __('Message', 'moloni_es') ?></a></th>
-            <th><a><?= __('Context', 'moloni_es') ?></a></th>
-        </tr>
-        </thead>
+    <form method="post" action='<?= admin_url('admin.php?page=molonies&tab=logs') ?>'>
+        <table class='wp-list-table widefat fixed striped posts'>
+            <thead>
+            <tr>
+                <th><a><?= __('Date', 'moloni_es') ?></a></th>
+                <th><a><?= __('Level', 'moloni_es') ?></a></th>
+                <th><a><?= __('Message', 'moloni_es') ?></a></th>
+                <th><a><?= __('Context', 'moloni_es') ?></a></th>
+            </tr>
+            <tr>
+                <th>
+                    <input
+                        type="date"
+                        class="inputOut ml-0"
+                        name="filter_date"
+                        value="<?= $_GET['filter_date'] ?? $_POST['filter_date'] ?? '' ?>"
+                    >
+                </th>
+                <th>
+                    <?php $options = LogLevel::getForRender() ?>
 
-        <?php if (!empty($logs) && is_array($logs)) : ?>
-            <?php foreach ($logs as $log) : ?>
+                    <select name="filter_level">
+                        <?php $filterLevel = $_GET['filter_level'] ?? $_POST['filter_level'] ?? '' ?>
+
+                        <option value='' selected><?=
+                            __('Choose an option', 'moloni_es') ?>
+                        </option>
+
+                        <?php foreach ($options as $option) : ?>
+                            <option
+                                value='<?= $option['value'] ?>'
+                                <?= $filterLevel === $option['value'] ? 'selected' : '' ?>
+                            >
+                            <?= $option['label'] ?>
+                            </option>
+                        <?php endforeach; ?>
+                    </select>
+                </th>
+                <th>
+                    <input
+                        type="text"
+                        class="inputOut ml-0"
+                        name="filter_message"
+                        value="<?= $_GET['filter_message'] ?? $_POST['filter_message'] ?? '' ?>"
+                    >
+                </th>
+                <th>
+                    <button type="submit" name="submit" id="submit" class="button button-primary">
+                        <?= __('Search', 'moloni_es') ?>
+                    </button>
+                </th>
+            </tr>
+            </thead>
+
+            <?php if (!empty($logs) && is_array($logs)) : ?>
+                <?php foreach ($logs as $log) : ?>
+                    <tr>
+                        <td>
+                            <?= $log['created_at'] ?>
+                        </td>
+                        <td>
+                            <?php
+                            $logLevel = $log['log_level'] ?? '';
+                            ?>
+
+                            <div class="chip <?= LogLevel::getClass($logLevel) ?>">
+                                <?= LogLevel::getTranslation($logLevel) ?>
+                            </div>
+                        </td>
+                        <td>
+                            <?= $log['message'] ?>
+                        </td>
+                        <td>
+                            <?php $logContext = htmlspecialchars($log['context']) ?>
+
+                            <button type="button" class="button action"
+                                    onclick="Moloni.Logs.openContextDialog(<?= $logContext ?>)">
+                                <?= __("See", 'moloni_es') ?>
+                            </button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else : ?>
                 <tr>
-                    <td>
-                        <?= $log['created_at'] ?>
-                    </td>
-                    <td>
-                        <?= LogLevel::getTranslation($log['log_level'] ?? '') ?>
-                    </td>
-                    <td>
-                        <?= $log['message'] ?>
-                    </td>
-                    <td>
-                        <?php $logContext = htmlspecialchars($log['context']) ?>
-
-                        <button type="button" class="button action"
-                                onclick="Moloni.Logs.openContextDialog(<?= $logContext ?>)">
-                            <?= __("See", 'moloni_es') ?>
-                        </button>
+                    <td colspan="4">
+                        <?= __('No records found!', 'moloni_es') ?>
                     </td>
                 </tr>
-            <?php endforeach; ?>
-        <?php else : ?>
-            <tr>
-                <td colspan="4">
-                    <?= __('No records found!', 'moloni_es') ?>
-                </td>
-            </tr>
-        <?php endif; ?>
+            <?php endif; ?>
 
-        <tfoot>
-        <tr>
-            <th><a><?= __('Date', 'moloni_es') ?></a></th>
-            <th><a><?= __('Level', 'moloni_es') ?></a></th>
-            <th><a><?= __('Message', 'moloni_es') ?></a></th>
-            <th><a><?= __('Context', 'moloni_es') ?></a></th>
-        </tr>
-        </tfoot>
-    </table>
+            <tfoot>
+            <tr>
+                <th><a><?= __('Date', 'moloni_es') ?></a></th>
+                <th><a><?= __('Level', 'moloni_es') ?></a></th>
+                <th><a><?= __('Message', 'moloni_es') ?></a></th>
+                <th><a><?= __('Context', 'moloni_es') ?></a></th>
+            </tr>
+            </tfoot>
+        </table>
+    </form>
 
     <div class="tablenav bottom">
         <div class="alignleft actions">
